93bb774d7ec3de744d60ae351d19d77c93090d31
Luiz Eduardo Franco <60003877+lEduFranco@users.noreply.github.com>
2021-03-15 11:26:43 -0300

    Criado imputs edit

---
+++ web/src/components/InputEdit/styles.ts
@@ -0,0 +1,78 @@
+import styled, { css } from 'styled-components';
+
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #e8e8ea;
+  border-radius: 15px;
+  padding: 16px;
+  width: 100%;
+
+  border: 2px solid #3172b7;
+  color: #3f1227;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+    ${(props) =>
+      props.isErrored &&
+      css`
+        border-color: #c53030;
+      `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #d19111;
+      border-color: #d19111;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #d19111;
+    `}
+
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #3f1227;
+
+    &::placeholder {
+      color: #a8a8a8;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #f5f5f5;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`;
